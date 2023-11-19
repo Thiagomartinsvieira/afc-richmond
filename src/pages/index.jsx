df09cdd82eba8e65d94be6bc710dd490d04ba78c
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-11-19 17:04:25 -0300

    created website introduction

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,26 @@
 import Link from "next/link";
-import styles from '../styles/Intruduction.module.css'
+import styles from '../styles/Intruduction.module.css';
 
 export default function Home() {
   return (
     <div className={styles.introduction}>
-      <h2>AFC Richmond</h2>
-      <div>
-        <h2>Project create by Thiago Martins</h2>
+      <div className={styles.headerSection}>
+        <h2>AFC Richmond</h2>
+        <h2>Project created by Thiago Martins</h2>
       </div>
 
-      <div className={styles.introduction2}>
-        <img src="https://t2.tudocdn.net/640244?w=1920&h=1440"
-          alt="afc Richmond"
+      <div className={styles.imageSection}>
+        <img
+          src="https://t2.tudocdn.net/640244?w=1920&h=1440"
+          alt="AFC Richmond"
+          className={styles.featuredImage}
         />
-        <Link href='/home'>go to website</Link>
-      </div>
 
+        <div className={styles.linkSection}>
+          <Link href='/home'>Go to website</Link>
+          <Link href='/become'>Become a fan member</Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
