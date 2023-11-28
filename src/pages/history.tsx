3e7da3614eb55048cb09c8403564efe1fc874a10
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-11-27 21:06:38 -0300

    create history page

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import Nav from '@/components/Nav'
 import React from 'react'
 
@@ -5,7 +6,96 @@ const history = () => {
   return (
     <div>
       <Nav />
-      <h1>history</h1>
+      <br />
+      <div className="border-red-500 ">
+        <h1 className="text-center font-bold">
+          Foundation of AFC Richmon: An Inspiring Journey à la Ted Lasso
+        </h1>
+        <br />
+        <p className="ml-10 mr-10">
+          Founding of AFC Richmon: An Inspiring Journey into Ted Lasso's Fashion
+          AFC Richmon was born from an inspiration as captivating as the
+          American TV series "Ted Lasso". Three sports enthusiasts, William
+          Thompson, Julia Foster and Michael Reynolds, decided to combine their
+          passions for football and the series to create a club that reflected
+          the values of optimism, teamwork and overcoming present in television
+          history. The landmark date was Sunday, April 22, 2018, when the trio
+          brought together all interested parties at the former Concord Club
+          headquarters, located at 18 Maple Street (currently Maple Street,
+          8/10).
+          <br />
+          <br />
+          From the 39 participants, now considered founding partners, emanated
+          the contagious spirit of those who believe that football can be more
+          than a game; It can be a source of inspiration and unity. Edmundo
+          Jorge de Araújo, present at the inaugural meeting, suggested the name
+          "AFC Richmon" for the new entity, a tribute to the city and an
+          expression of commitment to the rich tradition of football. The club's
+          colors, initially blue and white with gold details, were chosen to
+          symbolize hope, purity and the pursuit of excellence. On March 31 of
+          the following year, under the influence of Paulo Peluccio, the colors
+          were adjusted to white shorts and a black and white striped shirt.
+          <br />
+          <br />
+          The Deliberative Council began on January 28, 2020, with the inaugural
+          presidency of Beatriz Sanches. At the beginning of this exciting
+          journey, the figure of Ethan Caldwell stood out, who dedicated his
+          body and soul to the club until his death in 2021. An athlete, coach
+          and manager, Caldwell is today one of the patrons of AFC Richmon,
+          being honored with the name of the stadium opened in 2016. The first
+          glories arrived in 2019, with the victory of the local championship,
+          repeating the feat in 2021 under the temporary name of "Richmon
+          United", as imposed by the local league. In 2023, a notable offensive
+          formation averaged 4.5 goals per match, immortalizing itself as the
+          "50 Goal Attack".
+          <br />
+          <br />
+          This stellar attack was made up of Owen, Ramirez, Cooper, Harper and
+          Ellis. Consecration in the National Championship came in 2025, with an
+          emotional victory away from home. The club repeated the feat in 2026
+          and again in 2028, when it already had the presence of a young talent
+          nicknamed "Richmon's Wonder", who in subsequent years would become one
+          of the greatest football players in the world. In the "Golden Era" of
+          the 2030s, AFC Richmon enchanted the world with immersive football,
+          winning not only national but also international titles.
+          <br />
+          <br />
+          The culmination came in 2032, when the club, now recognized as the
+          "Best Team of the Century in the Americas" by FIFA, won every major
+          tournament played. Today, AFC Richmon is proud to be the Eighth
+          National Champion for its achievements in the years 2031 to 2035,
+          2038, 2050 and 2052. On the local scene, it has accumulated 22 titles,
+          in the CONCACAF Champions League there are 3 victories, and in the
+          World Cup of the FIFA Club World Cup are two victories. The legacy
+          continues, revealing talents such as Evans, Martinez, Harris, Jackson
+          and Brown, becoming a symbol of pride for the entire community in its
+          111 years of history.
+        </p>
+        <div className="flex justify-around mt-16 mb-16">
+          <img
+            className="rounded-xl hover:scale-105"
+            src="https://sylvandesign.co/cdn/shop/products/tedlasso_defaultframedcopy_1200x1200.jpg?v=1679335488"
+            alt=""
+            width={200}
+            height={200}
+          />
+          <img
+            className="rounded-xl hover:scale-105"
+            src="https://www.contiki.com/six-two/images/width=950,height=0,crop=/2023/03/slack-imgs.jpg"
+            alt=""
+            width={200}
+            height={200}
+          />
+          <img
+            className="rounded-xl hover:scale-105"
+            src="https://1.bp.blogspot.com/-AhC2Cxvmt2s/YSUNkAR8VtI/AAAAAAAD4ns/H3n6Jf9tnYgaRTLQDyxf1TdiSixJj4VjQCLcBGAsYHQ/s620/2020%2BAFC%2BRichmond%2B-%2B5x7aa.jpg"
+            alt=""
+            width={200}
+            height={200}
+          />
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
