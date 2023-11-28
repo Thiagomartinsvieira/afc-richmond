61f3983fc9201c4c1fb6edc5c2b86f7f1d0b6a10
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-11-27 21:06:25 -0300

    create footer model

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="bg-gray-800 text-white py-8">
+        <div className="container mx-auto flex items-center justify-around ">
+          <div className="flex items-center space-x-4">
+            <img
+              src="https://i.pinimg.com/originals/03/87/0f/03870f01e4595fe300ccc1a328043153.png"
+              alt=""
+              className="w-14 h-14"
+            />
+            <h2 className="text-xl font-semibold">AFC Richmond</h2>
+          </div>
+
+          <div className="space-x-4">
+            <a href="#" className="hover:text-gray-400">
+              Home
+            </a>
+            <a href="#" className="hover:text-gray-400">
+              Home
+            </a>
+            <a href="#" className="hover:text-gray-400">
+              Home
+            </a>
+            <a href="#" className="hover:text-gray-400">
+              Home
+            </a>
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <a href="#" className="text-xl hover:text-gray-400">
+              <i className="fab fa-facebool"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
