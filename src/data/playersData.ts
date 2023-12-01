92602177f1128e9791e6067e3949316e6c3cb8cd
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-11-30 21:50:11 -0300

    create players database

---
+++ src/data/playersData.ts
@@ -0,0 +1,127 @@
+export const playersSquad = [
+  {
+    name: "Tom O'Brien",
+    position: 'Goalkeeper',
+    imageUrl: './images/players/Tom_OBrien.webp',
+    playerNumber: 1,
+  },
+
+  {
+    name: 'Thierry Zoreauxd',
+    position: 'Goalkeeper',
+    imageUrl: './images/players/Thierry_Zoreaux.webp',
+    playerNumber: 81,
+  },
+
+  {
+    name: 'Martin De Maat',
+    position: 'Goalkeeper',
+    imageUrl: './images/players/DeMaat.webp',
+    playerNumber: 22,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Gareth Canterbury',
+    position: 'Left Back',
+    imageUrl: './images/players/Gareth_Canterbury.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+
+  {
+    name: 'Arlo Dixon',
+    position: 'Right Back',
+    imageUrl: './images/players/Arlo_Dixon.webp',
+    playerNumber: 2,
+  },
+]
