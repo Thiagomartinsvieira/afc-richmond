39c87224b63195a12704bd971f95f14af7061bb4
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-12-05 14:14:07 -0300

    created sponsor footer

---
+++ src/components/Footer/SponsorFooter.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const SponsorFooter = () => {
+  return (
+    <div className="mx-auto text-center flex items-center">
+      <footer className="bg-gray-600 w-screen border-t-4 border-b-4 border-yellow-300">
+        <div className="space-x-4 flex justify-around">
+          <img
+            src="https://static.wixstatic.com/media/636de3_18bac9d3d4944086a616bcadfe59bddb~mv2.png/v1/fill/w_360,h_216,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/primary-logo_3x.png"
+            alt=""
+            width="50pxspo"
+          />
+          <img
+            src="https://cdn.icon-icons.com/icons2/2699/PNG/512/apple_logo_icon_168588.png"
+            alt=""
+            width="60px"
+          />
+          <img
+            src="https://cdn-icons-png.flaticon.com/256/732/732084.png"
+            alt=""
+            width="50px"
+          />
+          <img
+            src="https://sep-bucket-prod.s3.amazonaws.com/wp-content/uploads/2021/08/logo-gatorade-peq-v02.png"
+            alt=""
+          />
+          <img src="https://i.imgur.com/eh3iv7q.png" alt="" width="60px" />
+          <img
+            src="https://sm.ign.com/ign_br/screenshot/default/b57zgnfk7wpa07vlbace6dkn302ki5k4dupu6fef2-ccubhrfou-licc03bh_b4hg.png"
+            alt=""
+            width="60px"
+          />
+          <img
+            src="https://1000logos.net/wp-content/uploads/2017/09/Nandos-Logo.png"
+            alt=""
+            width="60px"
+          />
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default SponsorFooter
