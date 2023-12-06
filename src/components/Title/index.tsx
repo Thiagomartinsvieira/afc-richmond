8471d91c6c3a5bc2348fcdb31426f60f40b72cd2
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-12-06 14:06:15 -0300

    created optional subtitle

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,9 +1,15 @@
 interface TitleProps {
   title: string
+  subtitle?: string
 }
 
 const Title = (props: TitleProps) => {
-  return <div className="text-center font-extrabold m-10">{props.title}</div>
+  return (
+    <div className="text-center font-extrabold m-10">
+      {props.title}
+      <div className="text-center mt-8">{props.subtitle}</div>
+    </div>
+  )
 }
 
 export default Title
