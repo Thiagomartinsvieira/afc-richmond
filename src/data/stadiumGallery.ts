77b149b7f05d61ce54e0bb6dc77637222fd19a71
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-12-07 19:19:33 -0300

    stadium photo gallery

---
+++ src/data/stadiumGallery.ts
@@ -0,0 +1,46 @@
+export const stadiumGallery = [
+  {
+    name: 'Nelson Road Stadium',
+    imageUrl:
+      'https://cdn.theathletic.com/app/uploads/2023/04/04090257/GettyImages-1478734996-scaled-e1680613440728.jpg',
+  },
+  {
+    name: 'Nelson Road Stadium',
+    imageUrl:
+      'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltfb0fc9da3e02a958/61730944f723b310a30de7c3/736ecf4a3bf2fee81764321deda353d7c52f981a.jpg?format=webp',
+  },
+  {
+    name: 'Nelson Road Stadium',
+    imageUrl:
+      'https://storage.googleapis.com/moviemaps/img/1szg.cejfc1.940.jpg',
+  },
+  {
+    name: 'Nelson Road Stadium',
+    imageUrl:
+      'https://pbs.twimg.com/ext_tw_video_thumb/1407006338475913221/pu/img/QQmdZ2KJ1Zxs4sNr.jpg',
+  },
+  {
+    name: 'Nelson Road Stadium',
+    imageUrl:
+      'https://groundhopperguides.com/wp-content/uploads/2021/07/selhurst-park-ted-lasso-1.png',
+  },
+  {
+    name: 'Nelson Road Stadium',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/London_Selhurst_Park_crystal_palace_stadium_aerialview.JPG/640px-London_Selhurst_Park_crystal_palace_stadium_aerialview.JPG',
+  },
+  {
+    name: 'Nelson Road Stadium',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1WM8I6-XegiGiIrAQn8nKaHkPQA3gCKSRGQ&usqp=CAU',
+  },
+  {
+    name: 'Nelson Road Stadium',
+    imageUrl: 'https://miro.medium.com/v2/resize:fit:1144/0*A4X0BorulVlTFIX1',
+  },
+  {
+    name: 'Nelson Road Stadium',
+    imageUrl:
+      'https://preview.redd.it/so-who-is-zava-why-is-his-name-on-different-team-kits-and-v0-b5sbgx70kska1.png?width=640&crop=smart&auto=webp&s=b7913bc685d1f683a69e9842e3f346c5df93ad86',
+  },
+]
