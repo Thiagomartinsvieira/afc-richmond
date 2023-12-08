42f642364708769df568ae9389214f930fe0714b
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-12-08 19:27:22 -0300

    created slider component

---
+++ src/components/ImageSlider/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { arrowLeft, arrowRight } from '../icons/icons'
+
+const ImageSlider = ({ images }) => {
+  const [currentImage, setCurrentImage] = useState(0)
+
+  const nextImage = () => {
+    setCurrentImage((prev) => (prev === images.length - 1 ? 0 : prev + 1))
+  }
+
+  const prevImage = () => {
+    setCurrentImage((prev) => (prev === 0 ? images.length - 1 : prev - 1))
+  }
+
+  return (
+    <div className="relative w-full max-w-screen-2xl mx-auto overflow-hidden">
+      <button
+        className="absolute top-1/2 transform -translate-y-1/2 left-10
+         text-white text-3xl z-10"
+        onClick={prevImage}
+      >
+        {arrowLeft}
+      </button>
+      <img
+        src={images[currentImage]}
+        alt={`Slide ${currentImage + 1}`}
+        className="w-full h-auto transition-transform transform translate-x-0"
+      />
+      <button
+        className="absolute top-1/2 transform -translate-y-1/2 right-10
+         text-white text-3xl z-10"
+        onClick={nextImage}
+      >
+        {arrowRight}
+      </button>
+    </div>
+  )
+}
+
+export default ImageSlider
