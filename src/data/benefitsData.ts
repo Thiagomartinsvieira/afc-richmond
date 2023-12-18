3b4ccef8f046a94fef81516867f04e3765fd258b
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-12-18 20:40:01 -0300

    created database with benefits card

---
+++ src/data/benefitsData.ts
@@ -0,0 +1,30 @@
+export const benefitsData = [
+  {
+    title: 'Richmond+',
+    about:
+      'Discounts and advantages on our partner network. By saving on your purchases, your plan may even be free',
+  },
+  {
+    title: 'Richmond Store',
+    about: '10% discount on all products, plus special offers and promotions.',
+  },
+  {
+    title: 'Ticket',
+    about: 'Minimum 50% discount and priority purchase.',
+  },
+  {
+    title: 'Rewards',
+    about:
+      'Redemption and raffle for exclusive products and unforgettable experiences.',
+  },
+  {
+    title: 'Content',
+    about:
+      'Interviews, behind-the-scenes videos, polls and much more for you to experience Santos more closely.',
+  },
+  {
+    title: 'Vote',
+    about:
+      'Help build the future of your club. All members over 18 years of age have the right to vote after 3 years of association',
+  },
+]
