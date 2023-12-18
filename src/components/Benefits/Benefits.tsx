880b068ce08b24c7fb6c86f126e628caafdcb8eb
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-12-18 20:39:32 -0300

    card benefits model finalized

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -1,75 +1,61 @@
 import React from 'react'
 import { arrowRight } from '../icons/icons'
+import BenefitsCard, { BenefitsCardProps } from '../Card/BenefitsCard'
+import { benefitsData } from '@/data/benefitsData'
 
 const Benefits = () => {
+  const chunkArray = (array: BenefitsCardProps[], chunkSize: number) => {
+    const result = []
+    for (let i = 0; i < array.length; i += chunkSize) {
+      result.push(array.slice(i, i + chunkSize))
+    }
+    return result
+  }
+
+  const benefitsChunks = chunkArray(benefitsData, 3)
+
   return (
-    <div className="flex justify-start my-8">
+    <div className="flex justify-start my-8 flex-wrap mx-5">
       <img
         src="https://www.asumidsouth.edu/wp-content/uploads/2021/10/Greyhounds_dog_head.png"
         alt=""
-        style={{ width: 60, height: 50 }}
+        style={{ width: 100, height: 65 }}
       />
-      <div className="ml-2 flex-col">
-        <h2>Benefits</h2>
-        <h3>No Santos, todo sócio é Rei</h3>
-        <hr className="w-auto mx-auto border-gray-500 my-4" />
+      <div className="flex flex-col mx-2">
+        <h2 className="font-bold text-yellow-500 text-xl">Benefits</h2>
+        <h3 className="font-semibold">
+          At AFC Richmond, every fan is the star of the show.
+        </h3>
+      </div>
+      <div className="ml-2 text-center">
+        <hr
+          className="w-auto mx-auto border-gray-500 h-1 bg-gradient-to-r 
+        from-blue-500 to-green-500 my-4"
+        />
         <p>
-          Só quem é Sócio Rei tem prioridade e desconto para comprar ingressos,
-          vantagens em nossos parceiros, possibilidade de participar de
-          experiências incríveis, acesso a conteúdos exclusivos e muito mais.
+          Only Richmonites Greyhounds fan members enjoy priority and exclusive
+          discounts when purchasing tickets, exclusive benefits across our
+          partner network, the opportunity to participate in incredible
+          experiences, access to exclusive content and much more. Become a fan
+          member and experience the best of the Richmonites Greyhounds universe!
         </p>
-
-        <div className="flex flex-wrap justify-center items-center m-4">
-          <div className="p-6 w-56 h-56 bg-gray-200 m-4 md:w-1/4">
-            <h3 className="font-extrabold text-center">Santos+</h3>
-            <p className="text-xs">
-              Descontos e vantagens em nossa rede de parceiros. Economizando nas
-              suas compras, seu plano pode até sair de graça.
-            </p>
+        {benefitsChunks.map((chunk, chunkIndex) => (
+          <div
+            key={chunkIndex}
+            className="flex justify-evenly items-center w-full mb-4"
+          >
+            {chunk.map((benefit, index) => (
+              <BenefitsCard key={index} {...benefit} />
+            ))}
           </div>
-          <div className="p-6 w-56 h-56 bg-gray-200 m-4 md:w-1/4">
-            <h2 className="font-extrabold text-center">Santos Store</h2>
-            <p className="text-xs">
-              10% de desconto em todos os produtos, além de ofertas e promoções
-              especiais.
-            </p>
-          </div>
-          <div className="p-6 w-56 h-56 bg-gray-200 m-4 md:w-1/4">
-            <h3 className="font-bold text-center">Ingresso</h3>
-            <p className="text-xs">
-              Mínimo de 50% de desconto e prioridade na compra.
-            </p>
-          </div>
-          <div className="p-6 w-56 h-56 bg-gray-200 m-4 md:w-1/4">
-            <h2 className="font-bold text-center">Recompensas</h2>
-            <p className="text-xs">
-              Resgate e sorteio de produtos exclusivos e experiências
-              inesquecíveis.
-            </p>
-          </div>
-          <div className="p-6 w-56 h-56 bg-gray-200 m-4 md:w-1/4">
-            <h2 className="font-bold text-center">Conteúdo</h2>
-            <p className="text-xs">
-              Entrevistas, vídeos de bastidores, enquetes e muito mais para você
-              viver o Santos mais de perto.
-            </p>
-          </div>
-          <div className="p-6 w-56 h-56 bg-gray-200 m-4 md:w-1/4">
-            <h2 className="font-bold text-center">Voto</h2>
-            <p className="text-xs">
-              Ajude a construir o futuro do seu clube. Todos os sócios maiores
-              de 18 anos têm direito a voto após 3 anos de associação.
-            </p>
-          </div>
-        </div>
-        <br />
+        ))}
         <div className="flex flex-col text-center justify-center items-center">
           <span className="mb-2">
-            E aí! Não dá pra perder esse tanto de benefícios, né?
+            And there! You can't miss out on all these benefits, right?{' '}
           </span>
           <p className="mb-2">
-            Torne-se sócio-torcedor do Santos F.C., um verdadeiro{' '}
-            <span className="text-yellow-600">Sócio Rei</span>!
+            Become an AFC Richmond fan, a true{' '}
+            <span className="text-yellow-600">elite experience</span>!{' '}
           </p>
           <button
             className="flex text-center justify-center border border-yellow-500
