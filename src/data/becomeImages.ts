76dbf187e70243d6c2c036828ac421637dd4d086
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-12-25 14:07:28 -0300

    defined become images

---
+++ src/data/becomeImages.ts
@@ -0,0 +1,81 @@
+export const BecomeImages = [
+  {
+    imageAlt: '1',
+    imageUrl:
+      'https://urbanpitch.com/wp-content/uploads/2021/08/210804_SKC_IB_14.jpg',
+  },
+  {
+    imageAlt: '2',
+    imageUrl:
+      'https://urbanpitch.com/wp-content/uploads/2021/08/210804_SKC_IB_33.jpg',
+  },
+  {
+    imageAlt: '',
+    imageUrl: 'https://i.ytimg.com/vi/68ZDkOADx-Y/maxresdefault.jpg',
+  },
+  {
+    imageAlt: '',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwep9Ko5OfVsUWBvqksuRxAVF60vhTNKOSHg&usqp=CAU',
+  },
+  {
+    imageAlt: '',
+    imageUrl:
+      'https://pbs.twimg.com/media/EfZAu8OXsAAmpXW?format=jpg&name=4096x4096',
+  },
+  {
+    imageAlt: '',
+    imageUrl:
+      'https://images.squarespace-cdn.com/content/v1/6000b64e47de5d00e78abdc1/e9bbdc60-ca6c-4691-83e1-b8e6726dff6c/TedLasso_LAFC_01706F.jpg',
+  },
+  {
+    imageAlt: '',
+    imageUrl:
+      'https://adversio.files.wordpress.com/2021/08/ted-lasso-203.jpg?w=1200',
+  },
+
+  {
+    imageAlt: '',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXaZCb3C3g2OkxMj7q_OoxSiHUeuNYqKNrzNnvh-Q1YQDG28xjGli2wQU-1kGc0YxJUkw&usqp=CAU',
+  },
+
+  {
+    imageAlt: '',
+    imageUrl:
+      'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt0bbe5f1d9f34d2d8/617022b878bb2210fdd07a15/e9250ede101a88f3d9a6cb240c5c2e908a119f31.jpg',
+  },
+
+  {
+    imageAlt: '',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_TN_FzML81t5c3U6WCgmIbmjEr9ghvHUIoNX-PtZoumnuma3zTtUWriDcbsAuAQcm7wI&usqp=CAU',
+  },
+
+  {
+    imageAlt: '',
+    imageUrl:
+      'https://prod-media.beinsports.com/image/1679140539441_a7861d68-1676-4a6a-b38f-0478e6e39e48.jpg',
+  },
+  {
+    imageAlt: '',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsdH6957M7am9rRKh-HpVggRQsppqeBPFcyA&usqp=CAU',
+  },
+
+  {
+    imageAlt: '',
+    imageUrl:
+      'https://metro.co.uk/wp-content/uploads/2023/03/26-Ted-Lasso-Airbnb-Annette-Badland-Portrait-Credit-Henry-Woide-17ff.jpg?quality=90&strip=all&zoom=1&resize=480%2C320',
+  },
+  {
+    imageAlt: '',
+    imageUrl:
+      'https://c8.alamy.com/comp/2RCJP01/birmingham-uk-15th-july-2023-15th-july-2023-edgbaston-birmingham-england-vitality-blast-t20-league-cricket-finals-day-essex-eagles-versus-hampshire-hawks-fans-wearing-afc-richmond-shirts-made-famous-by-the-ted-lasso-television-series-credit-action-plus-sports-imagesalamy-live-news-2RCJP01.jpg',
+  },
+  {
+    imageAlt: '',
+    imageUrl:
+      'https://static1.colliderimages.com/wordpress/wp-content/uploads/2023/05/ted_lasso_s3_ep_11_coaches.jpg',
+  },
+]
