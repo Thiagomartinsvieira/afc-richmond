df80461525c5655e17ee6279486eec0d6f625c8a
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-12-26 16:52:46 -0300

    Implemented alternation between "Players Squad", "Team Idols" and "Staff"

--- src/pages/players.tsx
+++ src/pages/players.tsx
@@ -3,35 +3,65 @@ import Nav from '../components/Nav'
 import PlayerCard from '@/components/Card/PlayerCard'
 import Title from '../components/Title'
 import { playersSquad } from '@/data/playersData'
+import { idolsData } from '@/data/idolsData'
+import { staffData } from '@/data/staffData'
 import Footer from '@/components/Footer'
 
 const Players = () => {
   const [showIdols, setShowIdols] = useState(false)
-  const dataShow = showIdols ? idolsData : playersSquad
+  const [showStaff, setShowStaff] = useState(false)
+
+  const dataToShow = showStaff
+    ? staffData
+    : showIdols
+      ? idolsData
+      : playersSquad
 
   const positions = Array.from(
-    new Set(playersSquad.map((player) => player.position)),
+    new Set(dataToShow.map((player) => player.position)),
   )
 
-  const handleToogleData = () => {
-    setShowIdols(!showIdols)
+  const handleToogleData = (dataType) => {
+    if (dataType === 'idols') {
+      setShowIdols(!showIdols)
+      setShowStaff(false)
+    } else if (dataType === 'staff') {
+      setShowStaff(!showStaff)
+      setShowIdols(false)
+    } else {
+      setShowIdols(false)
+      setShowStaff(false)
+    }
   }
 
   return (
     <div>
       <Nav />
       <Title title="Players squad" />
-      <h1 className="text-center font-bold cursor-pointer">
-        Main Male | Team Idols | staff
-        <button onClick={handleToogleData} className="tex"></button>
+      <h1
+        className="text-center font-bold cursor-pointer"
+        onClick={handleToogleData}
+      >
+        {showIdols ? 'Back to Players Squad' : 'View team Idols'}
       </h1>
+      <div className="flex justify-center mt-4 space-x-4">
+        <button onClick={() => handleToogleData('players')} className="btn">
+          Players Squad
+        </button>
+        <button onClick={() => handleToogleData('idols')} className="btn">
+          Team Idols
+        </button>
+        <button onClick={() => handleToogleData('staff')} className="btn">
+          Staff
+        </button>
+      </div>
 
       {positions.map((position, index) => (
         <div key={index}>
           <h2 className="text-2xl font-semibold mt-4">{position}</h2>
           <hr className="mb-2" />
           <div className="flex flex-wrap justify-center items-center">
-            {playersSquad
+            {dataToShow
               .filter((player) => player.position === position)
               .map((player, i) => (
                 <PlayerCard key={i} {...player} />
