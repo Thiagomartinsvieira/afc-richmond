b7ff4618023a063d8113243d02b4200c25ff74a9
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-12-28 21:40:40 -0300

    create news data for afc richmond news

---
+++ src/data/newsData.ts
@@ -0,0 +1,47 @@
+export const newsData = [
+  {
+    imageUrl: 'images/banner/baner1.png',
+    article:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    imageUrl: 'images/banner/baner1.png',
+    article:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    imageUrl: 'images/banner/baner1.png',
+    article:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    imageUrl: 'images/banner/baner1.png',
+    article:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    imageUrl: 'images/banner/baner1.png',
+    article:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    imageUrl: 'images/banner/baner1.png',
+    article:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    imageUrl: 'images/banner/baner1.png',
+    article:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    imageUrl: 'images/banner/baner1.png',
+    article:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    imageUrl: 'images/banner/baner1.png',
+    article:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+]
