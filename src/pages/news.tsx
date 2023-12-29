f7ed5f7078c5e3762ec7d7e1900ca58b1bdd26dc
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-12-28 21:41:01 -0300

    finish news page

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,13 +1,39 @@
+import Footer from '@/components/Footer'
 import Nav from '@/components/Nav'
+import NewsList from '@/components/NewsList'
+import { searchIcon } from '@/components/icons/icons'
 import React from 'react'
 
-const news = () => {
+const News = () => {
   return (
     <div>
       <Nav />
-      <h1>news</h1>
+      <div className="flex flex-col mb-10">
+        <img
+          src="https://media.santosfc.com.br/wp-content/uploads/2023/09/Nova-Arte-Pagina-Noticias.png"
+          alt=""
+          className="mb-10"
+        />
+        <h2 className="font-bold text-3xl text-center mb-4">News</h2>
+        <div className="flex flex-row items-center justify-center">
+          <input
+            type="search"
+            placeholder="Search"
+            className="bg-white w-11/12 border border-yellow-400 rounded-l-lg p-2 sm:w-10/12
+            lg:w-10/12 xl:w-7/12"
+          />
+          <div
+            className="bg-yellow-400 rounded-r-lg cursor-pointer p-2
+           px-4 border border-yellow-400"
+          >
+            {searchIcon}
+          </div>
+        </div>
+        <NewsList />
+      </div>
+      <Footer />
     </div>
   )
 }
 
-export default news
+export default News
