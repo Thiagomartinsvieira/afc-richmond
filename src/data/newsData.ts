6a972fcf79ec83e68aea26b786de06637212a2e4
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-01-04 21:22:29 -0300

    imported all images from the news component

--- src/data/newsData.ts
+++ src/data/newsData.ts
@@ -1,95 +1,79 @@
 export const newsData = [
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/KUHNbRlOS9fgkH6c6fPzyQ/1478x832.webp',
+    imageUrl: 'images/news/7_478x832.webp',
     article:
       'American football coach Ted Lasso is hired by a wealthy divorcée to coach English soccer team AFC Richmond.',
   },
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/GLMDjO768_3vMGe5ThO7JA/1478x832.webp',
+    imageUrl: 'images/news/7_478x832.webp',
     article:
       "It's Ted's first day of coaching, and fans aren't happy. He makes little headway but remains undeterred as the team plays its first match.",
   },
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/X4MkDp7-RcI9_JwXPE9HAg/1478x832.webp',
+    imageUrl: 'images/news/9_478x832.webp',
     article:
       'When Jamie refuses to train, Ted turns to talented new signing Dani Rojas-and the team is struck by an age-old curse.',
   },
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/EYIQRuwVSZhDAqxauZiu-Q/1478x832.webp',
+    imageUrl: 'images/news/10_478x832.webp',
     article:
       'Ted and Roy are both having relationship problems. Ted consults the Diamond Dogs while Roy lays it on the line with Keeley.',
   },
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/GlE_dUe1yfdZ1OeXliTawA/1478x832.webp',
+    imageUrl: 'images/news/11_478x832.webp',
     article:
       'Richmond plays a climactic match that will determine the fates of Ted and his club.',
   },
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/0FjOWTVD0lo47GTFEh8XdA/1478x832.webp',
+    imageUrl: 'images/news/12_478x832.webp',
     article:
       'Ted is fired up that the new team dynamic seems to be working. But will they have a chance in the quarter final?',
   },
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/-4xpdhSr1sIqdChBmXSXeA/1478x832.webp',
+    imageUrl: 'images/news/13_478x832.webp',
     article:
       'Ted and Dr. Sharon realize they may need to meet each other halfway. Tensions rise as the team prepares for the FA Cup semi-final against Man City.',
   },
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/0hALIr8RiYxXXOfvl7CIgg/1478x832.webp',
+    imageUrl: 'images/news/14_478x832.webp',
     article:
       "Richmond has his last chance to win the promotion as Ted deals with the fallout from Trent Crimm's painfully honest expose.",
   },
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/fY63DW0_9DcBJDknp56Ptg/1478x832.webp',
+    imageUrl: 'images/news/15_478x832.webp',
     article:
       "The newly promoted AFC Richmond face mockery as pundits predict they'll finish dead last.",
   },
-
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/U5aHTIol_X0iGfd2xOcOUA/1478x832.webp',
+    imageUrl: 'images/news/1_478x832.webp',
     article:
       "The time has come for the debut of Richmond's new acquisition, with all the consequences entails. Hannah attends an unusual session, while Ted is perplexed by his personal situation.",
   },
 
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/8oR7HNUSji_bNXD-F6oUyg/1478x832.webp',
+    imageUrl: 'images/news/2_478x832.webp',
     article:
       'Everyone is feeling the pressure as Richmond prepare to play West Han. Ted reunites with an old friend.',
   },
 
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/vB_S85ulfCGLnOJOkrmtIQ/1478x832.webp',
+    imageUrl: 'images/news/3_478x832.webp',
     article:
       "In a difficult phase, AFC Richmond has to face the league's titan and conflicts and complications on and off the court accumulate.",
   },
 
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/44A0VMGBcA2Aj8qsGn7usQ/1478x832.webp',
+    imageUrl: 'images/news/4_478x832.webp',
     article:
       "The Greyhounds try a new strategy that has everyone thinking outside the box. Sam prepares to host a VIP guest at Ola's.",
   },
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/gJ2ZVAqQY_ctyTlBIdPuTQ/1478x832.webp',
+    imageUrl: 'images/news/5_478x832.webp',
     article:
       "Colin and Isaac's friendship is tested. Roy is asked to do a press conference.",
   },
   {
-    imageUrl:
-      'https://is1-ssl.mzstatic.com/image/thumb/wynv9UM-Ko3Ipxe8q4iKxg/1478x832.webp',
+    imageUrl: 'images/news/6_478x832.webp',
     article:
       'An unexpected guest makes Ted nervous. When Richmond travels to Manchester for a big match, Roy and Keeley are worried about Jamie.',
   },
