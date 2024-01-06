773a9ee329e076001a52ecac26ae8e730b165485
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-01-06 20:41:13 -0300

    change image cover and text color font to white

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,46 +2,63 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen relative">
-      <img
-        src="https://t2.tudocdn.net/640244?w=1920&h=1440"
-        alt="AFC Richmond"
-        className="w-full h-full rounded-lg shadow-lg mb-4"
-      />
-
+    <div className="relative h-screen">
       <div
-        className="absolute 
-      top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-      text-white text-center"
-      >
-        <div className="my-10">
-          <h2 className="text-orange-500 py-2 text-4xl font-bold">
-            AFC Richmond
-          </h2>
-          <h2 className="bg-gray-900 text-xl my-2">
-            Project created by Thiago Martins
-          </h2>
-        </div>
+        className="absolute top-0 left-0 w-full h-full"
+        style={{
+          backgroundImage: `url('images/cover/Afc_cover.png')`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      ></div>
 
-        <div className="flex flex-col space-y-4">
-          <Link href="/home">
-            <button
-              className="text-lg bg-blue-500 text-white py-2 px-4 rounded-md
-             hover:bg-blue-700 transition"
-            >
-              Go to website
-            </button>
+      <div className="absolute top-0 right-0 m-4 text-white text-right">
+        <div className="my-10 flex flex-col items-center">
+          <Link href={'/home'}>
+            <img
+              src="images/logos/Richmond_logo.png"
+              alt="AFC Richmond logo"
+              className="w-28 h-auto"
+            />
           </Link>
-          <Link href="/become">
-            <button
-              className="text-lg bg-green-500 text-white py-2 px-4 rounded-md 
-            hover:bg-green-700 transition"
-            >
-              Become a fan member
-            </button>
+          <Link href={'/home'} className="font-extrabold">
+            Site
           </Link>
         </div>
       </div>
+
+      <div className="absolute top-36 right-0 m-4 text-white text-right">
+        <div className="my-10 flex flex-col items-center">
+          <Link href={'/become'}>
+            <img
+              src="images/logos/Greyhounds_dog.png"
+              alt="AFC Richmond logo"
+              className="w-24 h-auto"
+            />
+          </Link>
+          <Link href={'/become'} className="font-extrabold">
+            Become
+          </Link>
+        </div>
+      </div>
+
+      <div className="absolute top-60 right-0 m-4 text-white text-right">
+        <div className="my-10 flex flex-col items-center">
+          <Link href={'/shop'}>
+            <img
+              src="https://s3-us-west-2.amazonaws.com/sportshub2-uploads-prod/files/sites/1037/2019/12/23165553/logo_outline-1.png"
+              alt="AFC Richmond logo"
+              className="w-24 h-auto"
+            />
+          </Link>
+          <Link href={'/shop'} className="font-extrabold">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <h1 className="absolute bottom-3 left-1/2 transform -translate-x-1/2 text-red-100">
+        Project created by Thiago Martins Vieira
+      </h1>
     </div>
   )
 }
