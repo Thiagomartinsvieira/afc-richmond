d7566c4b6c50ee4a196b9f43d720b64fbfb9f407
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-01-11 12:09:57 -0300

    added more quick navigation icons

--- src/components/Nav/VerticalVideoNav.tsx
+++ src/components/Nav/VerticalVideoNav.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { FaShoppingBag, FaUserPlus, FaYoutube } from 'react-icons/fa'
 import { MdStadium } from 'react-icons/md'
+import { FaNewspaper, FaPeopleGroup } from 'react-icons/fa6'
 
 const VerticalVideoNav = () => {
   return (
@@ -18,9 +19,22 @@ const VerticalVideoNav = () => {
             </Link>
           </li>
           <li className="mb-10">
-            <Link href={'/become'} className="text-white cursor-pointer ">
+            <Link
+              href={
+                'https://tv.apple.com/br/show/ted-lasso/umc.cmc.vtoh0mn0xn7t3c643xqonfzy'
+              }
+              target="_blank"
+              className="text-white cursor-pointer "
+            >
               <div className="flex flex-col-reverse items-center transform transition-transform hover:scale-110">
                 RichmondTv <FaYoutube />
+              </div>
+            </Link>
+          </li>
+          <li className="mb-10">
+            <Link href={'/players'} className="text-white cursor-pointer">
+              <div className="flex flex-col-reverse items-center transform transition-transform hover:scale-110">
+                Players <FaPeopleGroup />
               </div>
             </Link>
           </li>
@@ -28,6 +42,13 @@ const VerticalVideoNav = () => {
             <Link href={'/stadium'} className="text-white cursor-pointer">
               <div className="flex flex-col-reverse items-center transform transition-transform hover:scale-110">
                 Nelson Road <MdStadium />
+              </div>
+            </Link>
+          </li>
+          <li className="mb-10">
+            <Link href={'/news'} className="text-white cursor-pointer">
+              <div className="flex flex-col-reverse items-center transform transition-transform hover:scale-110">
+                News <FaNewspaper />
               </div>
             </Link>
           </li>
