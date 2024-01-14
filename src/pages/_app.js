c985ba14f6d6f37ac1c77032d625cd5a972c079e
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-01-14 16:51:05 -0300

    add Toast Container for toast messsagens in project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import { ToastContainer } from 'react-toastify'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </>
+  )
 }
