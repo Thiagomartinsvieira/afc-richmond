a7c234d1acdc2c88e31a1012c21c15a9557d3ddd
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-02-06 21:39:41 -0300

    chore(frontEnd): added conditional rendering based on ticket page

--- frontend/src/components/Nav/SecondaryNav.tsx
+++ frontend/src/components/Nav/SecondaryNav.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const SecondaryNav = () => {
@@ -6,31 +7,34 @@ const SecondaryNav = () => {
     {
       id: 1,
       title: '/images/logos/Arsenal',
-      //   date: '04-12-23',
       teamLogo: '/images/logos/ARSENAL.png',
-      isHome: true,
+      atHome: true,
     },
     {
       id: 2,
       title: 'Aston vila',
-      //   date: '14-12-23',
       teamLogo: '/images/logos/aston_villa_17994.png',
       isHome: false,
     },
     {
       id: 3,
-      title: 'Manchester city',
-      //   date: '20-12-23',
-      teamLogo: '/images/logos/MANCHESTER-CITY.png',
+      title: 'Manchester united',
+      teamLogo: '/images/logos/Manchester-United-icon.png',
       isHome: true,
     },
   ]
 
   return (
     <div>
-      <nav className="flex flex-row justify-around items-center bg-white p-1">
+      <nav
+        className="flex flex-row justify-around items-center 
+      bg-white p-1"
+      >
         <div className="flex text-center">
-          <h2 className="text-lg font-normal mr-10 text-center text-black">
+          <h2
+            className="text-lg font-normal mr-10 text-center 
+          text-black"
+          >
             Upcoming Games
           </h2>
           <Image
@@ -43,28 +47,51 @@ const SecondaryNav = () => {
         <ul className="flex space-x-4 justify-center items-center mt-1">
           {upcomingGames.map((game) => (
             <li key={game.id} className="mb-2 flex items-center">
-              <Image
-                width={50}
-                height={50}
-                src="/images/logos/Richmond_logo.png"
-                alt=""
-                className="mr-2"
-              />
-              <p className="text-black pr-2 pl-2">x</p>
-              <Image
-                src={game.teamLogo}
-                width={50}
-                height={50}
-                alt={`Logo ${game.title}`}
-                className="w-8 h-8 mr-2"
-              />
+              {game.atHome ? (
+                <>
+                  <Image
+                    width={40}
+                    height={50}
+                    src="/images/logos/Afc-richmond.webp"
+                    alt="Afc richmond logo"
+                  />
+                  <p className="text-black mx-1">vs</p>
+                  <Image
+                    src={game.teamLogo}
+                    width={40}
+                    height={50}
+                    alt={`Logo ${game.title}`}
+                  />
+                </>
+              ) : (
+                <>
+                  <Image
+                    src={game.teamLogo}
+                    width={40}
+                    height={50}
+                    alt={`Logo ${game.title}`}
+                  />
+                  <p className="text-black mx-1">vs</p>
+                  <Image
+                    width={40}
+                    height={50}
+                    src="/images/logos/Afc-richmond.webp"
+                    alt="Afc richmond logo"
+                  />
+                </>
+              )}
               <a href="#" className="text-blue-500 hover:underline"></a>
             </li>
           ))}
         </ul>
-        <p className="text-black bg-yellow-300 p-1 cursor-pointer rounded-lg">
-          Tickets
-        </p>
+        <Link href="/tickets">
+          <p
+            className="text-black bg-yellow-300 p-1 cursor-pointer 
+          rounded-lg"
+          >
+            Tickets
+          </p>
+        </Link>
       </nav>
     </div>
   )
