7d60573f76d4c47f2646f4a2ea9aafa4ea44d0aa
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-02-08 17:41:15 -0300

    chore(frontEnd): building icon added

--- frontend/src/components/icons/icons.tsx
+++ frontend/src/components/icons/icons.tsx
@@ -433,3 +433,27 @@ export const starIcon = (
     <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
   </svg>
 )
+
+export const constructionIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className="w-6 h-6"
+  >
+    <rect x="2" y="6" width="20" height="8" rx="1" />
+    <path d="M17 14v7" />
+    <path d="M7 14v7" />
+    <path d="M17 3v3" />
+    <path d="M7 3v3" />
+    <path d="M10 14 2.3 6.3" />
+    <path d="m14 6 7.7 7.7" />
+    <path d="m8 6 8 8" />
+  </svg>
+)
