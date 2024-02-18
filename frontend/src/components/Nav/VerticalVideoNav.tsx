e5f001a06b31f87309d45978ecb7597d478a0df3
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-02-18 15:59:23 -0300

    feat(frontEnd): Added the react-use library to the project

--- frontend/src/components/Nav/VerticalVideoNav.tsx
+++ frontend/src/components/Nav/VerticalVideoNav.tsx
@@ -6,15 +6,18 @@ import { FaNewspaper, FaPeopleGroup } from 'react-icons/fa6'
 const VerticalVideoNav = () => {
   return (
     <div
-      className="absolute left-0 top-0 h-full p-4 bg-black bg-opacity-50 text-white
-     z-10"
+      className="absolute left-0 top-0 h-full p-4 bg-black bg-opacity-50 
+    text-white z-10 flex justify-center items-center"
     >
       <nav>
-        <ul className="flex flex-col mt-10">
+        <ul className="flex flex-col items-center">
           <li className="mb-10">
             <Link href={'/become'} className="text-white cursor-pointer">
-              <div className="flex flex-col-reverse items-center transform transition-transform hover:scale-110">
-                Become <FaUserPlus />
+              <div
+                className="flex flex-col-reverse items-center transform 
+              transition-transform hover:scale-110"
+              >
+                Become <FaUserPlus size={30} />
               </div>
             </Link>
           </li>
@@ -26,36 +29,42 @@ const VerticalVideoNav = () => {
               target="_blank"
               className="text-white cursor-pointer "
             >
-              <div className="flex flex-col-reverse items-center transform transition-transform hover:scale-110">
-                RichmondTv <FaYoutube />
+              <div
+                className="flex flex-col-reverse items-center justify-center 
+                transform transition-transform hover:scale-110"
+              >
+                RichmondTv <FaYoutube size={30} />
               </div>
             </Link>
           </li>
           <li className="mb-10">
             <Link href={'/players'} className="text-white cursor-pointer">
               <div className="flex flex-col-reverse items-center transform transition-transform hover:scale-110">
-                Players <FaPeopleGroup />
+                Players <FaPeopleGroup size={30} />
               </div>
             </Link>
           </li>
           <li className="mb-10">
             <Link href={'/stadium'} className="text-white cursor-pointer">
               <div className="flex flex-col-reverse items-center transform transition-transform hover:scale-110">
-                Nelson Road <MdStadium />
+                Nelson Road <MdStadium size={30} />
               </div>
             </Link>
           </li>
           <li className="mb-10">
             <Link href={'/news  '} className="text-white cursor-pointer">
               <div className="flex flex-col-reverse items-center transform transition-transform hover:scale-110">
-                News <FaNewspaper />
+                News <FaNewspaper size={30} />
               </div>
             </Link>
           </li>
           <li>
             <Link href={'/shop'} className="text-white cursor-pointer">
-              <div className="flex flex-col-reverse flex-col items-center transform transition-transform hover:scale-110">
-                Richmond Store <FaShoppingBag />
+              <div
+                className="flex flex-col-reverse items-center 
+              transform transition-transform hover:scale-110"
+              >
+                Richmond Store <FaShoppingBag size={30} />
               </div>
             </Link>
           </li>
