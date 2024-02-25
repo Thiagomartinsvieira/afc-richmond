f11e92e227889b453630d2a80a44bab2494a8192
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-02-25 19:12:48 -0300

    feat(frontEnd): added id to each news

--- frontend/src/data/newsData.ts
+++ frontend/src/data/newsData.ts
@@ -1,78 +1,93 @@
 export const newsData = [
   {
+    id: 1,
     imageUrl: 'images/news/7_478x832.webp',
     article:
       'American football coach Ted Lasso is hired by a wealthy divorcée to coach English soccer team AFC Richmond.',
   },
   {
+    id: 2,
     imageUrl: 'images/news/8_478x832.webp',
     article:
       "It's Ted's first day of coaching, and fans aren't happy. He makes little headway but remains undeterred as the team plays its first match.",
   },
   {
+    id: 3,
     imageUrl: 'images/news/9_478x832.webp',
     article:
       'When Jamie refuses to train, Ted turns to talented new signing Dani Rojas-and the team is struck by an age-old curse.',
   },
   {
+    id: 4,
     imageUrl: 'images/news/10_478x832.webp',
     article:
       'Ted and Roy are both having relationship problems. Ted consults the Diamond Dogs while Roy lays it on the line with Keeley.',
   },
   {
+    id: 5,
     imageUrl: 'images/news/11_478x832.webp',
     article:
       'Richmond plays a climactic match that will determine the fates of Ted and his club.',
   },
   {
+    id: 6,
     imageUrl: 'images/news/12_478x832.webp',
     article:
       'Ted is fired up that the new team dynamic seems to be working. But will they have a chance in the quarter final?',
   },
   {
+    id: 7,
     imageUrl: 'images/news/13_478x832.webp',
     article:
       'Ted and Dr. Sharon realize they may need to meet each other halfway. Tensions rise as the team prepares for the FA Cup semi-final against Man City.',
   },
   {
+    id: 8,
     imageUrl: 'images/news/14_478x832.webp',
     article:
       "Richmond has his last chance to win the promotion as Ted deals with the fallout from Trent Crimm's painfully honest expose.",
   },
   {
+    id: 9,
     imageUrl: 'images/news/15_478x832.webp',
     article:
       "The newly promoted AFC Richmond face mockery as pundits predict they'll finish dead last.",
   },
   {
+    id: 10,
     imageUrl: 'images/news/1_478x832.webp',
     article:
       "The time has come for the debut of Richmond's new acquisition, with all the consequences entails. Hannah attends an unusual session, while Ted is perplexed by his personal situation.",
   },
 
   {
+    id: 11,
     imageUrl: 'images/news/2_478x832.webp',
     article:
       'Everyone is feeling the pressure as Richmond prepare to play West Han. Ted reunites with an old friend.',
   },
 
   {
+    id: 12,
     imageUrl: 'images/news/3_478x832.webp',
     article:
       "In a difficult phase, AFC Richmond has to face the league's titan and conflicts and complications on and off the court accumulate.",
   },
 
   {
+    id: 13,
     imageUrl: 'images/news/4_478x832.webp',
     article:
       "The Greyhounds try a new strategy that has everyone thinking outside the box. Sam prepares to host a VIP guest at Ola's.",
   },
   {
+    id: 14,
     imageUrl: 'images/news/5_478x832.webp',
     article:
       "Colin and Isaac's friendship is tested. Roy is asked to do a press conference.",
   },
   {
+    id: 15,
     imageUrl: 'images/news/6_478x832.webp',
     article:
       'An unexpected guest makes Ted nervous. When Richmond travels to Manchester for a big match, Roy and Keeley are worried about Jamie.',
