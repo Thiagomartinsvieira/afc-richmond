f6391970d6c1b131b5eeeca62946049d757aa382
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-02-28 08:13:57 -0300

    feat(frontEnd): created conditional route for the news card

--- frontend/src/components/Card/NewsCard.tsx
+++ frontend/src/components/Card/NewsCard.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 interface NewsCardProps {
   article: string
   imageUrl: string
+  id: number
 }
 
 const NewsCard = (props: NewsCardProps) => {
@@ -11,14 +13,16 @@ const NewsCard = (props: NewsCardProps) => {
     <div className="flex flex-col mt-10 mx-auto">
       <div className="m-2 flex flex-col">
         <div className="flex" style={{ width: '310px' }}>
-          <Image
-            src={`/${props.imageUrl}`}
-            alt={`image of ${props.imageUrl}`}
-            width={400}
-            height={400}
-            objectFit="cover"
-            className="opacity-60 hover:opacity-100 rounded cursor-pointer"
-          />
+          <Link href={`/news/${props.id}`}>
+            <Image
+              src={`/${props.imageUrl}`}
+              alt={`image of ${props.imageUrl}`}
+              width={400}
+              height={400}
+              objectFit="cover"
+              className="opacity-60 hover:opacity-100 rounded cursor-pointer"
+            />
+          </Link>
         </div>
         <div style={{ width: '310px' }} className="mx-auto mt-2">
           <p className="text-xs my-4 text-center overflow-hidden">
