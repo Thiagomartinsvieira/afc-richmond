049321da77d72507b1a6b12f4e1eb3dd77dfcc2a
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-02-28 09:08:29 -0300

    Squashed commit of the following:
    commit e91f173f2851f4bc7a137c63c82c784a4b7733f7
    Author: Thiago Martins Vieira <thiagoalagadico@gmail.com>
    Date:   Wed Feb 28 09:05:21 2024 -0300
    
        chore(frontEnd): past the properties for dynamic navigation
    
    commit f932f7d8dec48d8b3aa28dea5e02ef7b9934e034
    Author: Thiago Martins Vieira <thiagoalagadico@gmail.com>
    Date:   Wed Feb 28 09:04:18 2024 -0300
    
        fix(frontEnd): fixed missing property type in NewsCardProps
    
    commit cf433dd33e9b95d7ccee1bfd73df7c4c8dfd5f06
    Author: Thiago Martins Vieira <thiagoalagadico@gmail.com>
    Date:   Wed Feb 28 09:01:42 2024 -0300
    
        feat(frontEnd): created dynamic route that receives the id and displays the news next to the title
    
    commit f6391970d6c1b131b5eeeca62946049d757aa382
    Author: Thiago Martins Vieira <thiagoalagadico@gmail.com>
    Date:   Wed Feb 28 08:13:57 2024 -0300
    
        feat(frontEnd): created conditional route for the news card
    
    commit 1bb469209bcd263e8b7a96d96961971f2fd2445a
    Author: Thiago Martins Vieira <thiagoalagadico@gmail.com>
    Date:   Wed Feb 28 08:12:33 2024 -0300
    
        feat(frontEnd): defined route for the news card
    
    commit 93c30111fe5f652dfed863621e7f5b597b97b370
    Author: Thiago Martins Vieira <thiagoalagadico@gmail.com>
    Date:   Tue Feb 27 21:30:14 2024 -0300
    
        feat(frontEnd): added title for news
    
    commit f11e92e227889b453630d2a80a44bab2494a8192
    Author: Thiago Martins Vieira <thiagoalagadico@gmail.com>
    Date:   Sun Feb 25 19:12:48 2024 -0300
    
        feat(frontEnd): added id to each news

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -7,6 +7,7 @@ import VerticalVideoNav from '@/components/Nav/VerticalVideoNav'
 import React, { useState } from 'react'
 import PlayersCarroucel from '@/components/Carroucel/PlayersCarroucel'
 import { useWindowSize } from 'react-use'
+import Link from 'next/link'
 
 const Home = () => {
   const { width } = useWindowSize()
@@ -39,30 +40,48 @@ const Home = () => {
           className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 
          lg:grid-cols-3 gap-4 items-center"
         >
-          <NewsCard
-            imageUrl="images/news/1_478x832.webp"
-            article="The time has come for the debut of Richmond's new acquisition, with all the consequences entails. Hannah attends an unusual session, while Ted is perplexed by his personal situation."
-          />
-          <NewsCard
-            imageUrl="images/news/2_478x832.webp"
-            article="Everyone is feeling the pressure as Richmond prepare to play West Han. Ted reunites with an old friend."
-          />
-          <NewsCard
-            imageUrl="images/news/3_478x832.webp"
-            article="In a difficult phase, AFC Richmond has to face the league's titan and conflicts and complications on and off the court accumulate."
-          />
-          <NewsCard
-            imageUrl="images/news/4_478x832.webp"
-            article="The Greyhounds try a new strategy that has everyone thinking outside the box. Sam prepares to host a VIP guest at Ola's."
-          />
-          <NewsCard
-            imageUrl="images/news/5_478x832.webp"
-            article="Colin and Isaac's friendship is tested. Roy is asked to do a press conference."
-          />
-          <NewsCard
-            imageUrl="images/news/6_478x832.webp"
-            article="An unexpected guest makes Ted nervous. When Richmond travels to Manchester for a big match, Roy and Keeley are worried about Jamie."
-          />
+          <Link className="flex" href="/news/15">
+            <NewsCard
+              id={10}
+              imageUrl="images/news/1_478x832.webp"
+              article="The time has come for the debut of Richmond's new acquisition, with all the consequences entails. Hannah attends an unusual session, while Ted is perplexed by his personal situation."
+            />
+          </Link>
+          <Link className="flex" href="/news/14">
+            <NewsCard
+              id={11}
+              imageUrl="images/news/2_478x832.webp"
+              article="Everyone is feeling the pressure as Richmond prepare to play West Han. Ted reunites with an old friend."
+            />
+          </Link>
+          <Link className="flex" href="/news/13">
+            <NewsCard
+              id={12}
+              imageUrl="images/news/3_478x832.webp"
+              article="In a difficult phase, AFC Richmond has to face the league's titan and conflicts and complications on and off the court accumulate."
+            />
+          </Link>
+          <Link className="flex" href="/news/12">
+            <NewsCard
+              id={13}
+              imageUrl="images/news/4_478x832.webp"
+              article="The Greyhounds try a new strategy that has everyone thinking outside the box. Sam prepares to host a VIP guest at Ola's."
+            />
+          </Link>
+          <Link className="flex" href="/news/11">
+            <NewsCard
+              id={14}
+              imageUrl="images/news/5_478x832.webp"
+              article="Colin and Isaac's friendship is tested. Roy is asked to do a press conference."
+            />
+          </Link>
+          <Link className="flex" href="/news/10">
+            <NewsCard
+              id={15}
+              imageUrl="images/news/6_478x832.webp"
+              article="An unexpected guest makes Ted nervous. When Richmond travels to Manchester for a big match, Roy and Keeley are worried about Jamie."
+            />
+          </Link>
         </div>
       </div>
 
